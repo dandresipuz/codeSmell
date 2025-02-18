7b150505905083650bccff96b6b118112d99eb06
Diego Andres Ipuz Garcia <daipuz@personalsoft.com>
2025-02-17 22:02:28 -0500

    Project_firts_Charge

---
+++ src/main/java/com/IngSoft/codeSmell/CodeSmellApplication.java
@@ -0,0 +1,13 @@
+package com.IngSoft.codeSmell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSmellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeSmellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IngSoft/codeSmell/CodeSmellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IngSoft.codeSmell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSmellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
